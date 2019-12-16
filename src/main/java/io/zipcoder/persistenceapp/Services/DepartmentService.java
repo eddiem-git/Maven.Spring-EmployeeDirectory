f60d62b7223c5dda8fd762b7e6c1e8d825e59315
eddie <eddiem9657@gmail.com>
2019-12-16 15:57:40 -0500

    EmployeesDirectory

--- src/main/java/io/zipcoder/persistenceapp/Services/DepartmentService.java
+++ src/main/java/io/zipcoder/persistenceapp/Services/DepartmentService.java
@@ -1,4 +1,33 @@
 package io.zipcoder.persistenceapp.Services;
 
+import io.zipcoder.persistenceapp.modules.Department;
+import io.zipcoder.persistenceapp.modules.Employee;
+import io.zipcoder.persistenceapp.repositories.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.DependsOn;
+
 public class DepartmentService {
+
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+
+    @Autowired
+    EmployeeService employeeService;
+
+    public Department getDepartment(Integer id){
+        return departmentRepository.findOne(id);
+    }
+
+    public Department createDepartment(Department department){
+        return departmentRepository.save(department);
+    }
+
+    public Department updateDepartment(Department department){
+        return departmentRepository.save(department);
+    }
+
+    public Department setManager(Long id, Long managerId){
+        return null;
+    }
 }
--- src/main/java/io/zipcoder/persistenceapp/Services/EmployeeService.java
+++ src/main/java/io/zipcoder/persistenceapp/Services/EmployeeService.java
@@ -1,4 +1,16 @@
 package io.zipcoder.persistenceapp.Services;
 
+import io.zipcoder.persistenceapp.modules.Employee;
+import io.zipcoder.persistenceapp.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class EmployeeService {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public Iterable<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+
 }
